1e31c091a341a04e1387dbdeecf03d2824fb0077
Sheep-y <your.sheepy@gmail.com>
2018-11-08 18:47:36 +0800

    Partial Rfc1924En and Z85Encoder javascript conversion.

---
+++ javascript/base85.js
@@ -0,0 +1,184 @@
+// Constants used in encoding and decoding
+const Power4 = 52200625; // 85^4
+const Power3 = 614125;  // 85^3
+const Power2 = 7225;   // 85^2
+
+const TypedArrayType = Object.getPrototypeOf( Uint8Array );
+const SharedArrayBufferType = () => { try { return SharedArrayBuffer; } catch ( err ) { return ArrayBuffer; } }
+
+// Convert all compatible input to DataView
+function MakeInput ( data ) {
+   if ( data instanceof DataView )
+      return data;
+   if ( typeof( data ) === 'string' )
+      data = new TextEncoder().encode( data ); // If string, encode as utf-8 and later convert to DataView below
+   if ( data instanceof TypedArrayType )
+      return new DataView( data.buffer );
+   if ( data instanceof ArrayBuffer || data instanceof SharedArrayBufferType )
+      return new DataView( data );
+   throw new TypeError( 'Unsupported Base85 input: must be string, TypedArray, DataView, or ArrayBuffer.' );
+}
+
+// Convert all compatible output to Uint8Array
+function MakeOutput ( data, size ) {
+   if ( ! data ) data = new Uint8Array( size() );
+   if ( data instanceof Uint8Array )
+      return data;
+   if ( data instanceof TypedArrayType )
+      return new Uint8Array( data.buffer );
+   if ( data instanceof DataView )
+      return new Uint8Array( data.buffer, data.byteOffset, data.byteLength );
+   if ( data instanceof ArrayBuffer || data instanceof SharedArrayBufferType )
+      return new Uint8Array( data );
+   throw new TypeError( 'Unsupported Base85 output: must be TypedArray, DataView, or ArrayBuffer' );
+}
+
+function BufToText ( data ) {
+   // data must be Uint8Array
+   return new TextDecoder( 'utf-8' ).decode( data );
+}
+
+function BufToCode ( data ) {
+   // data must be string or Uint8Array
+   if ( typeof( data ) === 'string' ) return data;
+   return new TextDecoder( 'ascii' ).decode( data );
+}
+
+function StringToMap ( data ) {
+   // data must be string, return Uint8Array map
+   return new TextEncoder().encode( data );
+}
+
+function CreateClass( baseObject, constructor ) {
+   if ( constructor == undefined ) constructor = function(){};
+   constructor.prototype = baseObject;
+   Object.assign( constructor, baseObject );
+   Object.setPrototypeOf( constructor, baseObject.__proto__ );
+   return constructor;
+}
+
+/** This is a skeleton class for encoding data using the Base85 encoding scheme,
+  * in the same style as Base64 encoder.
+  * Encoder instances can be safely shared by multiple threads.
+  */
+export const Base85Encoder = {
+
+   /** Calculate the max encoded byte length of a given input data. */
+   calcEncodedLength ( data ) {
+      data = MakeInput( data );
+      return ~~Math.ceil( data.byteLength * 1.25 );
+   },
+
+   /** Encode data.
+    * 1. If a String is supplied, a Base85 string of its utf-8 data will be returned.
+    * 2. If a TypedArray is supplied, a TypedArray of the Base85 data will be returned.
+    * 3. A TypedArray output buffer can also be supplied, in which case the number of Base85 data will be returned regardless of input type.
+    */
+   encode ( data, out ) {
+      const inputIsString = typeof( data ) === 'string', outputIsProveded = out ? true : false;
+      data = MakeInput( data );
+      out = MakeOutput( out, () => this.calcEncodedLength( data ) );
+      const len = this._encode( data, out );
+      if ( outputIsProveded ) return len;
+      if ( len !== out.length ) out = out.slice( 0, len );
+      if ( inputIsString ) return BufToCode( out );
+      return out;
+   },
+
+   /** Encode data into Base85 string. */
+   encodeToString ( data ) {
+      return BufToCode( this.encode( data ) );
+   },
+
+   /** Encode part of data as one block in reverse output order into output array.
+     * This is the strict algorithm specified by RFC 1924 for IP address encoding,
+     * when the data part is exactly 16 bytes (128 bits) long.
+     * Because the whole input data part is encoded as one big block,
+     * this is much less efficient than the more common encodings.
+     * Output is [ out, encoded_byte_count ]
+     */
+   encodeBlockReverse ( data, out ) {
+      throw 'Not implemented';
+      data = MakeInput( data );
+      const size = this.calcEncodedLength( data ), map = this.getEncodeMap();
+      out = MakeOutput( out, () => size );
+      let blockSum = BigInt( 0 ), b85 = BigInt( 85 );
+      for ( let e of data ) { // TODO: Old Uint8 code. Change to use DataView.
+         blockSum << 8;
+         blockSum += BigInt( e );
+      }
+      for ( let i = size - 1 ; i >= 0 ; i-- ) {
+         const mod = blockSum % b85;
+         blockSum = ( blockSum - mod ) / b85;
+         out[ i ] = map[ mod ];
+      }
+      return [ out, size ];
+   },
+
+   _encode () { throw new TypeError( '_encode() is not implemented by Base85Encoder' ); },
+   getEncodeMap () { throw new TypeError( 'getEncodeMap() is not implemented by Base85Encoder' ); },
+   getCharset() { return BufToCode( this.getEncodeMap() ); },
+};
+
+
+
+/* An encoder that does not support compression */
+export const Base85SimpleEncoder = { __proto__ : Base85Encoder,
+
+   _encodeDangling ( encodeMap, out, wi, buf, leftover ) {
+      let sum = new DataView( buf.buffer ).getUint32();
+      out[wi  ] = encodeMap[ parseInt( sum / Power4 ) ]; sum %= Power4;
+      out[wi+1] = encodeMap[ parseInt( sum / Power3 ) ]; sum %= Power3;
+      if ( leftover >= 2 ) {
+         out[wi+2] = encodeMap[ parseInt( sum / Power2 ) ]; sum %= Power2;
+         if ( leftover >= 3 )
+            out[wi+3] = encodeMap[ parseInt( sum / 85 ) ];
+      }
+      return leftover + 1;
+   },
+
+   _encode ( data, out ) {
+      const rlen = data.byteLength;
+      if ( rlen <= 0 ) return 0;
+      const encodeMap = this.getEncodeMap(), leftover = rlen % 4;
+      let ri = data.byteOffset, wi = 0;
+      for ( let loop = parseInt( rlen / 4 ) ; loop > 0 ; loop--, ri += 4, wi += 5 ) {
+         let sum = data.getUint32( ri );
+         out[wi  ] = encodeMap[ parseInt( sum / Power4 ) ]; sum %= Power4;
+         out[wi+1] = encodeMap[ parseInt( sum / Power3 ) ]; sum %= Power3;
+         out[wi+2] = encodeMap[ parseInt( sum / Power2 ) ]; sum %= Power2;
+         out[wi+3] = encodeMap[ parseInt( sum / 85 ) ];
+         out[wi+4] = encodeMap[ sum % 85 ];
+      }
+      if ( leftover == 0 ) return wi;
+      const buf = new Uint8Array( 4 );
+      buf.set( new Uint8Array( data.buffer.slice( ri ) ) );
+      return wi + this._encodeDangling( encodeMap, out, wi, buf, leftover );
+   },
+};
+
+/** This class encodes data in the Base85 encoding scheme using the character set described by IETF RFC 1924,
+  * but in the efficient algorithm of Ascii85 and Z85.
+  * This scheme does not use quotes, comma, or slash, and can usually be used in sql, json, csv etc. without escaping.
+  *
+  * @see https://tools.ietf.org/html/rfc1924
+  */
+export const Rfc1924Encoder = CreateClass( { __proto__ : Base85SimpleEncoder,
+   ENCODE_MAP : StringToMap( "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~" ),
+   getEncodeMap () { return this.ENCODE_MAP; },
+} );
+
+/** This class encodes data in the Base85 encoding scheme Z85 as described by ZeroMQ.
+  * This scheme does not use quotes or comma, and can usually be used in sql, json, csv etc. without escaping.
+  *
+  * @see https://rfc.zeromq.org/spec:32/Z85/
+  */
+export const Z85Encoder = CreateClass( { __proto__ : Base85SimpleEncoder,
+   ENCODE_MAP : StringToMap( "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-:+=^!/*?&<>()[]{}@%$#" ),
+   getEncodeMap () { return this.ENCODE_MAP; },
+} );
+
+export default {
+   getRfc1942Encoder() { return Rfc1924Encoder; },
+   getZ85Encoder() { return Z85Encoder; },
+};
---
+++ javascript/tests.js
@@ -0,0 +1,79 @@
+import Base85 from './base85.js';
+
+window.Base85 = Base85; // For browser console debug
+
+const rfcE = Base85.getRfc1942Encoder();
+//const rfcD = Base85.getRfc1942Decoder();
+const z85E = Base85.getZ85Encoder();
+//const z85D = Base85.getZ85Decoder();
+//const a85E = Base85.getAscii85Encoder();
+//const a85D = Base85.getAscii85Decoder();
+
+/////////// Generic Test Routines ///////////
+
+function StringToByte ( data ) { return new TextEncoder().encode( data ); }
+
+function testStrEncode ( assert, e, map ) {
+   for ( let i = 0 ; i < map.length ; i += 2 )
+      assert.equal( map[i+1], e.encode( map[i] ), "Encode " + map[i] );
+}
+
+function testStrDecode ( assert, d, map ) {
+   for ( let i = 0 ; i < map.length ; i += 2 ) {
+      assert.equal( map[i], d.decode( map[i+1] ), "Decode " + map[i+1] );
+      assert.deepEqual( map[i].getBytes( UTF_8 ), d.decode( map[i+1].getBytes( US_ASCII ) ), "Decode " + map[i+1] + " to bytes" );
+   }
+}
+
+function testByteEncode ( assert, e, map ) {
+   const origStr = map[ map.length - 2 ], codeStr = map[ map.length - 1 ];
+   let orig = StringToByte( origStr ), code = StringToByte( codeStr );
+   assert.equal( codeStr, e.encodeToString( orig ), "encodeToString" );
+   assert.deepEqual( code, e.encode( orig ), "Byte to byte encode" );
+   /*
+   let buf = Arrays.copyOf( orig, orig.length * 2 );
+   assert.deepEqual( code, e.encode( buf, 0, orig.length ), "Byte to byte encode offset 0" );
+   System.arraycopy( buf, 0, buf, 2, orig.length );
+   assert.deepEqual( code, e.encode( buf, 2, orig.length ), "Byte to byte encode offset 2" );
+   let output = new byte[ code.length + 2 ];
+   e.encode( orig, 0, orig.length, output, 0 );
+   assert.deepEqual( code, Arrays.copyOfRange( output, 0, code.length ), "Byte to byte direct encode offset 0" );
+   e.encode( buf, 2, orig.length, output, 2 );
+   assert.deepEqual( code, Arrays.copyOfRange( output, 2, code.length + 2 ), "Byte to byte direct encode offset 2" );
+   */
+}
+
+/////////// RFC Tests ///////////
+
+const rfcTests = [
+   "", "",
+   "A", "K>",
+   "AB", "K|%",
+   "ABC", "K|(_",
+   "ABCD", "K|(`B",
+   "ABCDE", "K|(`BMF",
+   "ABCDEF", "K|(`BMMe",
+   "ABCDEFG", "K|(`BMMg&",
+   "ABCDEFGH", "K|(`BMMg(R",
+   "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!#$%&()*+-;<=>?@^_`{|}~",
+      "FflSSG&MFiI5|N=LqtVJM@UIZOH55pPf$@(Q&d$}S6EqEVPa!sWoBn+X=-b1ZEkOHadLBXb#`}nd3qruBqb&&DJm;1J3Ku;KR{kzV0(Oheg",
+   "測試中", "=D4irsix$(tp",
+   "اختبارات", "*r(X8*s9p5*r(XB*r(X4",
+];
+
+/*
+QUnit.test( "RfcSpec", function ( assert ) {
+   byte[] addr = Inet6Address.getByName( "1080:0:0:0:8:800:200C:417A" ).getAddress();
+   String encoded = "4)+k&C#VzJ4br>0wv%Yp";
+   assert.equal( encoded, new String( rfcE.encodeBlockReverse( addr ), US_ASCII ), "Inet encode" );
+   assert.deepEqual( addr, rfcD.decodeBlockReverse( encoded.getBytes( US_ASCII ), "Inet encode" ) );
+} );
+*/
+
+QUnit.test( "RfcStrEncode", function( assert ) { testStrEncode( assert, rfcE, rfcTests ); } );
+//QUnit.test( "RfcStrDecode", function( assert ) { testStrDecode( assert, rfcD, rfcTests ); } );
+QUnit.test( "RfcEncode", function( assert ) { testByteEncode( assert, rfcE, rfcTests ); } );
+//QUnit.test( "RfcDecode", function( assert ) { testByteDecode( assert, rfcD, rfcTests ); } );
+//QUnit.test( "RfcRoundTrip", function( assert ) { testRoundTrip( assert, rfcE, rfcD ); } );
+//QUnit.test( "RfcWrongData", function( assert ) { testInvalidData( assert, rfcE, rfcD ); } );
+//QUnit.test( "RfcWrongLength", function( assert ) { testInvalidLength( assert, rfcE, rfcD ); } );
